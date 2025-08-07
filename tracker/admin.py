d2a8afd8f22ea9cec6eb1046d85b26509e93760b
Bob Belderbos <bobbelderbos@gmail.com>
2025-08-07 18:20:19 +0200

    model tweaks, created migration file, added models to admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Site, League
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "site",
+        "league",
+        "fee_paid",
+        "is_volunteer",
+        "mileage",
+        "mileage_paid",
+        "position",
+    )
+    search_fields = ("date", "site__name", "league__organization")
+
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ("name", "address")
+    search_fields = ("name",)
+
+
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ("organization", "assignor", "game_fee")
+    search_fields = ("organization", "assignor")
