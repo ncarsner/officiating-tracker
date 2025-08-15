c8fb4252f81af93cd5b0f3c19f1648c1ef6b5c38
Nicholas Carsner <nicholascarsner@gmail.com>
2025-08-15 08:02:58 -0500

    Updated forms, views. Add pre-commit.

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,17 +1,21 @@
 from django import forms
-from tracker.models import Game, Site, League
+
+from tracker.models import Game, League, Site
+
 
 class GameForm(forms.ModelForm):
     class Meta:
         model = Game
-        fields = ['date', 'site', 'league', 'position', 'fee_paid', 'is_volunteer', 'mileage', 'mileage_paid']
+        fields = ["date", "site", "league", "position"]
+
 
 class SiteForm(forms.ModelForm):
     class Meta:
         model = Site
-        fields = ['name', 'address']
+        fields = ["name", "address"]
+
 
 class LeagueForm(forms.ModelForm):
     class Meta:
         model = League
-        fields = ['organization', 'assignor', 'game_fee', 'description']
+        fields = ["organization", "assignor", "game_fee", "description"]
--- tracker/views.py
+++ tracker/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from tracker.models import Game, League, Site
+from django.shortcuts import get_object_or_404, render
+
 from tracker.forms import GameForm
+from tracker.models import Game
 
 
 # Create your views here.
 def game_list(request):
-    context = {}
     form = GameForm()
-    games = Game.objects.select_related('league', 'site').all()
-    context['games'] = games
-    context['title'] = 'Game List'
-    context['form'] = form
-    return render(request, 'game/list.html', context)
+    # TODO: handle successful form submission (e.g., redirect)
+    if request.method == "POST":
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+    # TODO: reload objects after db insert
+    games = Game.objects.select_related("league", "site").all()
+    context = {"games": games, "title": "Game List", "form": form}
+    return render(request, "game/list.html", context)
+
 
 def game_detail(request, pk):
     game = get_object_or_404(Game, pk=pk)
-    return render(request, 'game/detail.html', {'game': game})
+    return render(request, "game/detail.html", {"game": game})
