85861401135aa3baf979512f1f77b23c2ce1fffe
Nicholas Carsner <nicholascarsner@gmail.com>
2025-08-21 17:13:40 -0500

    Added views for create, edit, delete game.

--- tracker/views.py
+++ tracker/views.py
@@ -21,3 +21,35 @@ def game_list(request):
 def game_detail(request, pk):
     game = get_object_or_404(Game, pk=pk)
     return render(request, "game/detail.html", {"game": game})
+
+
+def game_create(request):
+    form = GameForm()
+    if request.method == "POST":
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # TODO: redirect to the game list or detail view
+    context = {"form": form, "title": "Add Game"}
+    return render(request, "game/add.html", context)
+
+
+def edit_game(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    form = GameForm(instance=game)
+    if request.method == "POST":
+        form = GameForm(request.POST, instance=game)
+        if form.is_valid():
+            form.save()
+            # TODO: redirect to the game list or detail view
+    context = {"form": form, "title": "Edit Game"}
+    return render(request, "game/edit.html", context)
+
+
+def delete_game(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    if request.method == "POST":
+        game.delete()
+        # TODO: redirect to the game list
+    context = {"game": game, "title": "Delete Game"}
+    return render(request, "game/delete.html", context)
